e2f5d49f2b36c4b52b3a10c86d17f94e0a8c4b0e
Md Ashik <mostofabilla@gmail.com>
2023-08-13 04:58:45 +0600

    deisgn login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,20 @@
 import React, {useState} from 'react';
-import {Form} from "@chakra-ui/theme/dist/components";
 import {
+  Box,
   Button,
-  ChakraProvider,
+  Container,
   FormControl,
-  FormHelperText,
-  FormLabel,
+  Heading,
   Input,
-  InputGroup, InputRightElement,
+  InputGroup,
+  InputRightElement,
   Stack
 } from "@chakra-ui/react";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
+  const apiEndpoint = process.env.API_ENDPOINT || 'https://smb-erp-api.mdashikjs.com/api/auth/login';
+
+  const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
@@ -23,51 +25,79 @@ const Login = () => {
     e.preventDefault();
     setLoading(true);
 
+    const payload = {
+      username: userName,
+      password: password,
+    }
+
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetch(apiEndpoint, {
         method: 'POST',
-        body: JSON.stringify({
-          email,
-          password,
-        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
       });
 
-      if (response.status === 200) {
+      if (response.ok) {
+        console.log(response)
+        setUserName('');
+        setPassword('');
         setLoading(false);
         window.location.href = '/';
       } else {
+        console.log(response)
         setLoading(false);
         setError('Invalid credentials');
       }
     } catch (error) {
       setLoading(false);
+      console.log(error)
       setError(error.message);
     }
   };
 
   return (
     <>
-      <Stack spacing={4}>
-        <FormControl isRequired>
-          <FormLabel>User name</FormLabel>
-          <Input type='text'/>
-        </FormControl>
-        <InputGroup size='md'>
-          <Input
-            pr='4.5rem'
-            type={show ? 'text' : 'password'}
-            placeholder='Enter password'
-          />
-          <InputRightElement width='4.5rem'>
-            <Button h='1.75rem' size='sm' onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-        <Button colorScheme='blue' onClick={handleSubmit}>Button</Button>
-      </Stack>
+      <Container maxW='2xl' minHeight="100vh" centerContent justifyContent="center">
+        <Box padding='8' borderRadius="5" bg='gray.300' color='black' maxW='lg' width='100%'>
+          <Stack spacing={4}>
+            <Heading as='h3' size='lg' align="center" paddingBottom="16px">
+              Login
+            </Heading>
+            <FormControl isRequired>
+              <Input type='text' size='lg'
+                     bg='white'
+                     placeholder='User name'
+                     value={userName}
+                     onChange={(e) => setUserName(e.target.value)}/>
+            </FormControl>
+            <InputGroup size='lg'>
+              <Input
+                pr='4.5rem'
+                bg='white'
+                type={show ? 'text' : 'password'}
+                placeholder='Enter password'
+                value={password} onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width='4.5rem'>
+                <Button size='sm' onClick={handleClick}>
+                  {show ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Button colorScheme='blue' size='lg'
+                    isLoading={loading}
+                    loadingText="Logging in..."
+                    isDisabled={!password || !userName}
+                    onClick={handleSubmit}>Login</Button>
+          </Stack>
+        </Box>
+      </Container>
+
     </>
   );
 };
+
 
 export default Login;
