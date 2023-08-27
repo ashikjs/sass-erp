95d4c9a618b2e3649016b167e784c920ce5d6827
Md Ashik <mostofabilla@gmail.com>
2023-08-28 05:45:24 +0600

    product category details

--- src/pages/product-categories/view/[id].tsx
+++ src/pages/product-categories/view/[id].tsx
@@ -7,7 +7,7 @@ import axiosApi from "src/app/utiles/axiosApi";
 const ProductCategoryDetails = () => {
   const router = useRouter();
   const {id} = router.query; // Get the id parameter from the URL
-  const [product, setProduct] = useState(null);
+  const [productCategory, setProductCategory] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -17,7 +17,7 @@ const ProductCategoryDetails = () => {
       // Fetch product details using axios
       axiosApi.get(`/categories/${id}`)
         .then((response) => {
-          setProduct(response.data);
+          setProductCategory(response.data);
           setLoading(false);
         })
         .catch((error) => {
@@ -40,13 +40,11 @@ const ProductCategoryDetails = () => {
   return (
     <Container maxWidth="xl">
       <Box p={4}>
-        {product ? (
+        {productCategory ? (
           <>
-            <Heading size="md" paddingBottom={2}>{product.name}</Heading>
-            <Text paddingBottom={1}><b>Price</b>: {product.price}</Text>
-            <Text paddingBottom={1}><b>Category</b>: {product.category?.name}</Text>
-            <Text paddingBottom={1}><b>Quantity</b>: {product.quantity}</Text>
-            <Text paddingBottom={1}><b>Description</b>: {product.description}</Text>
+            <Heading size="md" paddingBottom={2}>{productCategory.name}</Heading>
+            <Text paddingBottom={1}><b>Status</b>: {productCategory.status}</Text>
+            <Text paddingBottom={1}><b>Description</b>: {productCategory.description}</Text>
           </>
         ) : (
           <Text>No product found.</Text>
